1f36ebe7b91d1634718be1bb281f1ff4ceaeb7f0
CodyBrewer <brewer.cody.m@gmail.com>
2020-02-01 10:28:38 -0700

    set up reducer case for TOGGLE_COMPLETE

--- client/src/reducers/todoReducer.js
+++ client/src/reducers/todoReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, ON_CHANGE } from "../actions/types";
+import { ADD_TODO, ON_CHANGE, TOGGLE_COMPLETE } from "../actions/types";
 
 export const initialState = {
   todos: [
@@ -23,6 +23,19 @@ export const todoReducer = (state = initialState, { type, payload }) => {
     case ADD_TODO:
       const newTodo = { task: payload, id: Date.now() };
       return { ...state, todos: [...state.todos, newTodo], todo: "" };
+    case TOGGLE_COMPLETE:
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (todo.id === payload.id) {
+            return {
+              ...todo,
+              completed: !todo.completed
+            };
+          }
+          return todo;
+        })
+      };
     default:
       return state;
   }
